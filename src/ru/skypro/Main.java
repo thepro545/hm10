6e804ed7a7844b40c13aa58d7b4e32c27104c601
Max <49117015+thepro545@users.noreply.github.com>
2022-02-01 12:42:00 +0300

    Update Main.java

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -10,14 +10,6 @@ public class Main {
         Book alchemist = new Book("Alchemist", new Author("Paulo","Coelho"), 1988);
         Author RayBradbury = new Author("Ray", "Bradbury");
         Author PauloCoelho = new Author("Paulo","Coelho");
-//        System.out.println("fahrenheit451 about = bookName - " + fahrenheit451.getBookName() + ", authorName - " + fahrenheit451.getAuthorName().getFullName() + ", publishingYear - " + fahrenheit451.getPublishingYear());
-//        System.out.println("alchemist.bookName = " + alchemist.getBookName());
-//        System.out.println("alchemist.authorName = " + alchemist.getAuthorName().getFullName());
-//        System.out.println("alchemist.publishingYear = " + alchemist.getPublishingYear());
-//        System.out.println("RayBradbury.fullName = " + RayBradbury.getFullName());
-//        System.out.println("PauloCoelho.fullName = " + PauloCoelho.getFullName());
-//        alchemist.setPublishingYear(1990);
-//        System.out.println("alchemist.getPublishingYear() = " + alchemist.getPublishingYear());
 
 //Task HM10
         System.out.println(fahrenheit451);
